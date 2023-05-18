c14b94500193165da39c7bc4a3a7474b64a58e8d
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-18 22:44:30 +0100

    useSubmit custom hook

--- advanced-react-portfolio/src/components/ContactMeSection.jsx
+++ advanced-react-portfolio/src/components/ContactMeSection.jsx
@@ -1,7 +1,33 @@
+import {useEffect} from "react";
+import { useFormik } from "formik";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+  Textarea,
+  VStack,
+} from "@chakra-ui/react";
+import * as Yup from 'yup';
+import useSubmit from "../hooks/useSubmit";
+import {useAlertContext} from "../context/alertContext";
+import FullScreenSection from "./FullScreenSection"
+
+
 function ContactMeSection(){
     return(
         <>
-            <h1>Contact Me Section</h1>
+            <FullScreenSection
+                isDarkBackground
+                backgroundColor={"#512DA8"}
+                py={16}
+                spacing={8}
+            >
+            </FullScreenSection>
         </>
     )
 }
